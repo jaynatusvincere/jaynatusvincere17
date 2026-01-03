81df2547dd49ea3c3f558bc01e8b3d8b985d3f90
jaynatusvincere <jayforcooding@gmail.com>
2026-01-03 19:35:32 +0800

    fix build config & disable turbopack

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  reactStrictMode: false,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  swcMinify: false,
 };
 
 export default nextConfig;
+
